de1b1d2d91e9ecce8edd8b0af3621e99ce6e4860
andrewvakhniuk <vah.a.val@gmail.com>
2015-05-13 14:51:43 +0300

    4_prac

---
+++ 4prac_SpringAOP/src/main/java/com/vah/AOP/ExpectorInterface.java
@@ -0,0 +1,6 @@
+package com.vah.AOP;
+
+public interface ExpectorInterface {
+	public void expect(Integer number);
+    public String getResult();
+}
---
+++ 4prac_SpringAOP/src/main/java/com/vah/beans/PlayerInterface.java
@@ -0,0 +1,5 @@
+package com.vah.beans;
+
+public interface PlayerInterface {
+	public void play(Integer number);
+}
